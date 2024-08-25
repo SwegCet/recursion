66669e6645c22042dc5aa7d4c8319b83a9f38c4a
SwegCet <ethantruong0804@gmail.com>
2024-08-25 14:50:35 -0700

    Added comments for clarity

--- ms/script.js
+++ ms/script.js
@@ -1,15 +1,17 @@
 function mergeSort(array) {
     const low = 0; //the first of the array
     const high = array.length - 1; //the last of the array
-    if (array.length <= 1) {
+
+    if (array.length <= 1) { //If we can't split the subarrays anymore, we start the sorting & merging
         return array;
     }
 
     mid = Math.floor(array.length / 2);
 
-    const leftSubArray = array.slice(low, mid);
-    const rightSubArray = array.slice(mid, high + 1);
+    const leftSubArray = array.slice(low, mid); //Splits the main array into left half
+    const rightSubArray = array.slice(mid); //Splits main array into right half
 
+    //Return sorted Left-SubArray and Right-SubArray
     return merge(mergeSort(leftSubArray), mergeSort(rightSubArray));
 }
 
@@ -19,7 +21,7 @@ function merge(left, right) {
     let rightIndex = 0;
 
     //Compare the values from both subarrays
-    while (leftIndex < left.length && rightIndex < right.length) {
+    while (leftIndex < left.length && rightIndex < right.length) { //Loop while we're in the subarrays
         if (left[leftIndex] <= right[rightIndex]) {
             result.push(left[leftIndex]);
             leftIndex++;
@@ -29,8 +31,11 @@ function merge(left, right) {
             rightIndex++;
         }
     }
+    //Concat remaining elements from subarrays if we have any
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 let arrayValue = [3, 2, 1, 13, 8, 5, 0, 1]
 console.log(mergeSort(arrayValue));
+let secondArray = [105, 79, 100, 110];
+console.log(mergeSort(secondArray));
